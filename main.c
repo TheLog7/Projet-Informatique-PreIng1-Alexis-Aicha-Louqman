c722dda495c207972938eb7c622122a61dce4af2
BA Louqman <130762713+TheLog7@users.noreply.github.com>
2023-04-17 14:55:58 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-// Bienvenue dans le fichier main.c, c'est moi wesh
-//Test wesh
-
+// Bienvenue dans le fichier main.c
 int main(){
     printf("Hello World\n");
     printf("Je teste si je peux fusionner deux branches autres que main");
