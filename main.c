61035ed55ad693529dc8126db224aa2d0d70dc0e
louqmanba <ba.louqman@gmail.com>
2023-04-17 15:29:38 +0200

    Test
    Ok

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 // Bienvenue dans le fichier main.c
+// Test de l'app de bureau
 int main(){
     printf("Hello World\n");
     printf("Je teste si je peux fusionner deux branches autres que main");
