608163a7e1ab20afc3d2c34ea6905f17f7f9a251
BA Louqman <130762713+TheLog7@users.noreply.github.com>
2023-05-21 22:15:32 +0200

    Big update

---
+++ Branche test/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// Bienvenue dans le fichier main.c
+// Test de l'app de bureau
+int main(){
+    printf("Hello World\n");
+    printf("Je teste si je peux fusionner deux branches autres que main");
+    return 0;
+}
