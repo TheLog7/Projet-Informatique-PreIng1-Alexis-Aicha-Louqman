82924eabb170e7596ce7f1e4ed3d5f8e8d00febc
Xeliok <130991034+Xeliok@users.noreply.github.com>
2023-05-22 00:41:59 +0200

    Delete Branche test directory

--- Branche test/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-// Bienvenue dans le fichier main.c
-// Test de l'app de bureau
-int main(){
-    printf("Hello World\n");
-    printf("Je teste si je peux fusionner deux branches autres que main");
-    return 0;
-}
